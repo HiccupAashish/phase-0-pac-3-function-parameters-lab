f286b2a53133a3fc8f546ca3896bfbefe0cb5870
HiccupAashih <shahaashish22@gmail.com>
2022-02-24 21:08:46 +1100

    Three test passed one still to go

--- test/index-test.js
+++ test/index-test.js
@@ -1,4 +1,22 @@
 
+function introduction(name){
+  return (`Hi, my name is ${name}.`);
+}
+introduction("Aki")
+function introductionWithLanguage(name, language){
+ return (`Hi, my name is ${name} and I am learning to program in ${language}.`);
+}
+
+introductionWithLanguage("aki", "Ember.js")
+ introductionWithLanguage("Samip", "React")
+
+ function introductionWithLanguageOptional(name, language="Javascript"){
+    
+   return (`Hi, my name is ${name} and I am learning to program in ${language}.`);
+    }
+    introductionWithLanguageOptional("Grace", "Javascript")
+   
+
 describe('introduction(name)', function() {
   it('takes in an argument of a name and returns a phrase with that name using string interpolation', function() {
     expect(introduction("Aki")).toEqual("Hi, my name is Aki.");
@@ -7,7 +25,7 @@ describe('introduction(name)', function() {
 })
 
 describe('introductionWithLanguage(name, language)', function() {
-  it('takes in two arguments, a name and a language, and returns a phrase using those arguments', function() {
+  it('takes in two arguments,  name and a language, and returns a phrase using those arguments', function() {
     expect(introductionWithLanguage("Aki", "Ember.js")).toEqual("Hi, my name is Aki and I am learning to program in Ember.js.");
     expect(introductionWithLanguage("Samip", "React")).toEqual("Hi, my name is Samip and I am learning to program in React.");
   })
